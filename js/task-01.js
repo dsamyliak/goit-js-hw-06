7b438ee64388ee33077d60625700cd5b7e2da1e9
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-06 15:56:06 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+
+//random task
+//---------------------
+// let obj = {
+//  "0": 1,
+//  0: 3,
+// };
+
+// console.log(obj);
+// console.log(obj["0"]);
+// console.log(obj[0]);
+// alert( obj["0"] + obj[0] );
+//---------------------
