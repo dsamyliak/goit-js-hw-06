e901044514c06c01dbc80cb22b66e25470514682
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-16 18:03:45 +0300

    Add Task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", pressLogin);
+
+function pressLogin(e) {
+    e.preventDefault();
+    
+    const {
+        elements: { email, password }
+    } = e.currentTarget;
+
+    // console.log('e.currentTarget:', e.currentTarget);
+        
+    if (email.value === "" || password.value === "") {
+    return  alert("Все поля должны быть заполнены!");
+    }
+    
+    const formInfo = {};
+    const formData = new FormData(e.currentTarget);
+
+    formData.forEach((value, name) => {
+        formInfo[name] = value;
+     });
+    
+    console.log("formInfo:", formInfo);
+    e.currentTarget.reset();
+};
+
 
