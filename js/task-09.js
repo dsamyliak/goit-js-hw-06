5285c3cfedace360908c2600145d08d89110da1a
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-16 18:39:54 +0300

    Add Task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyColor: document.querySelector("body"),
+  spanColor: document.querySelector("span.color"),
+  changeColorBtn: document.querySelector("button.change-color"),
+};
+
+refs.changeColorBtn.addEventListener("click", btnClick);
+
+function btnClick () {
+  
+  refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  refs.spanColor.textContent = refs.bodyColor.style["background-color"];
+
+};
