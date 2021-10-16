fd8c3f2393e859d6b28516f3047f2e99a8869213
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-16 15:40:06 +0300

    Update Task 01

--- js/task-01.js
+++ js/task-01.js
@@ -2,16 +2,18 @@ const categoriesList = document.querySelector('ul#categories');
 console.log('Number of categories:', categoriesList.children.length);
 console.log('');
 
-const h2arrayCategories = categoriesList.querySelectorAll('.item');
+const arrayCategories = categoriesList.querySelectorAll('.item');
 
-for (let index = 0; index < h2arrayCategories.length; index++) {
-    console.log(`Category: ${h2arrayCategories[index].firstElementChild.textContent}`);
-    console.log(`Elements: ${categoriesList.children[index].querySelectorAll('li').length}`,);
-    if (index < h2arrayCategories.length-1) {
-        console.log("");
-    };
-};
+console.log(arrayCategories);
 
+arrayCategories.forEach ( element => {
+   
+    console.log(`Category: ${element.querySelector("h2").textContent}`);
+    console.log(`Elements: ${element.querySelectorAll('li').length}`,);
+    
+});
+
+// Old version
 // for (let index = 0; index < h2arrayCategories.length; index++) {
 //     console.log(`Category: ${h2arrayCategories[index].firstElementChild.textContent}`);
 //     console.log(`Elements: ${categoriesList.children[index].querySelectorAll('li').length}`,);
