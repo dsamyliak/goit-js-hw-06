7a397840358d6a608ead3bfb07d055bc6ac418f1
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-19 17:47:10 +0300

    Upd Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,68 +4,61 @@ function getRandomHexColor() {
 
 const divBoxes = document.querySelector("div#boxes");
 const inputDivNumber = document.querySelector("input");
-inputDivNumber.value = 1;
+
+const startWidth = 30;
+const startHeight = 30;
+let lastDeltaWidth = 0;
+let lastDeltaHeight = 0;
+let newDiv;
+let newDivs = [];
 
 // createBoxes(amount)
 
 const createBtn = document.querySelector("button[data-create]");
 
 function createBoxes(amount) {
-
-  destroyBoxes();
-
+  
   amount = inputDivNumber.value;
   console.log("create boxes", amount);
 
-  let startWidth = 30;
-  let startHeight = 30;
-  let newDiv;
+  if (amount === 1) {
+    newDiv = document.createElement("div");
+    newDiv.classList.add("created-boxes");
+    newDiv.style.backgroundColor = getRandomHexColor();
+    // newDiv.style.border = "inset 3px gray";
+    newDiv.style.width = `${startWidth}px`;
+    newDiv.style.height = `${startHeight}px`;
+    console.log(newDiv);
 
+    newDivs.push(newDiv);
+    return divBoxes.append(...newDivs);
 
-  // empty div, like margin
-  const emptyDiv = document.createElement("div");
-  emptyDiv.classList.add("created-boxes");
-  emptyDiv.style.height = "10px";
-  divBoxes.append(emptyDiv);
-  // 
+  } else {
+    for (let index = 0; index < amount; index++) {
   
-  // old version
-  // for (let index = 0; index < amount; index++) {
-
-  //   newDiv = document.createElement("div");
-  //   newDiv.classList.add("created-boxes");
-  //   newDiv.style.backgroundColor = getRandomHexColor();
-  //   newDiv.style.border = "inset 3px gray";
-  //   newDiv.style.width = `${startWidth}px`;
-  //   newDiv.style.height = `${startHeight}px`;
-  //   startWidth += 10;
-  //   startHeight += 10;
-    
-  //   divBoxes.append(newDiv);
-  //   console.log("divBoxes", divBoxes);
-
-  // };  
-  
-  let newDivs = [];
-
-for (let index = 0; index < amount; index++) {
-
   newDiv = document.createElement("div");
   newDiv.classList.add("created-boxes");
   newDiv.style.backgroundColor = getRandomHexColor();
   newDiv.style.border = "inset 3px gray";
-  newDiv.style.width = `${startWidth}px`;
-  newDiv.style.height = `${startHeight}px`;
-  startWidth += 10;
-  startHeight += 10;
+  newDiv.style.width = `${startWidth + lastDeltaWidth}px`;
+  newDiv.style.height = `${startHeight + lastDeltaHeight}px`;
+  lastDeltaWidth += 10;
+  lastDeltaHeight += 10;
   
   newDivs.push(newDiv);
   
   };
     
   divBoxes.append(...newDivs);
-  console.log('newDivs',newDivs);
-  console.log("divBoxes", divBoxes);
+  
+    console.log('newDivs', newDivs);
+    console.log("divBoxes", divBoxes);
+    
+  newDivs = [];
+  };
+    
+  inputDivNumber.value = "";
+
 };
 
 createBtn.addEventListener("click", createBoxes);
@@ -77,12 +70,17 @@ const destroyBtn = document.querySelector("button[data-destroy]");
 
 
 function destroyBoxes() {
-  const newDivs = document.querySelectorAll(".created-boxes");
+  let newDivs = document.querySelectorAll(".created-boxes");
   
   newDivs.forEach(newDiv => {
   newDiv.remove();
   });
+
+  inputDivNumber.value = "";
+  lastDeltaWidth = 0;
+  lastDeltaHeight = 0;
+  newDivs = [];
+    
 };
 
 destroyBtn.addEventListener("click", destroyBoxes);
-
